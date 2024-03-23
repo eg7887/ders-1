bf579e8b34702b406594fc55df05ca0f526a27e9
eg7887 <164669065+eg7887@users.noreply.github.com>
2024-03-23 21:29:55 +0300

    sözlük

---
+++ sozluk.py
@@ -0,0 +1,12 @@
+word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
+
+meme_dict = {
+            "CRINGE": "Garip ya da utandırıcı bir şey",
+            "LOL": "Komik bir şeye verilen cevap",
+            "SHEESH": "onaylamamak"
+            }
+            
+if word in meme_dict.keys():
+    print(meme_dict[word])
+else:
+    print("ben de bilmiyorum")
